84884b4c82a88450c2d560cec897561be37f4af0
Your Name <you@example.com>
2026-01-21 15:51:43 +0800

    add m20

---
+++ README.md
@@ -0,0 +1,62 @@
+
+# Wheel-Legged Robot Reinforcement Learning Library Dreamwaq
+
+
+## 🧩 Introduction
+
+This repository provides a **Reinforcement Learning (RL) framework** for wheel-legged robots.  
+It supports **simulation-based policy training**, **Sim2Sim validation**, and **Sim2Real deployment** on real robots such as **Unitree Go2** and **custom wheel-legged platforms**.
+
+### 🚀 Features
+- ✅ Supports vision-proprioception fusion reinforcement learning  
+- ✅ End-to-end pipeline for training, validation, and deployment  
+- ✅ Extensible to multiple robot platforms (Go2, m20, etc.)  
+- ✅ Supports custom reward functions and curriculum learning
+
+---
+
+## 🛠️ 使用指南
+### 0. 安装依赖
+python环境：3.8
+#### Isaacgym 安装
+
+```bash
+pip install -e ~/isaacgym/python
+```
+#### rsl 
+```bash
+pip install -e ./rsl_rl
+```
+#### legged_gym
+```bash
+pip install -e .
+```
+#### 山猫m20 已经部署实物
+
+## Use
+
+### 1. train 
+```bash
+cd legged_gym
+python legged_gym/scripts/train.py --task=m20 --headless
+```
+
+python legged_gym/scripts/train.py --task=go2_handstand --headless
+
+python legged_gym/scripts/train.py --task=go2_legstand --headless
+
+### 2. play policy
+```bash
+cd legged_gym
+python legged_gym/scripts/play.py --task=m20 --num_envs=50
+```
+
+python legged_gym/scripts/play.py --task=go2_handstand --num_envs=50
+
+python legged_gym/scripts/play.py --task=go2_legstand --num_envs=50
+
+### problem
+如果你发现轮子不转，而是抬腿，把urdf中的continuous改称revolute,上下限改一下-99999 99999，即可
+
+# thanks for the code 
+https://github.com/XinLang2019/Wheel_Legged_Gym
---
+++ deploy/deploy_mujoco/deploy_mujoco.py
@@ -0,0 +1,205 @@
+import time
+
+import mujoco.viewer
+import mujoco
+import numpy as np
+# from legged_gym import LEGGED_GYM_ROOT_DIR
+import torch
+import yaml
+import onnxruntime as ort
+import matplotlib.pyplot as plt
+
+DEPLOY_DIR = '/home/zju/Wheel_Legged_Gym-main'
+import keyboard  # 导入keyboard库
+from pynput import keyboard
+
+x_vel_cmd, y_vel_cmd, yaw_vel_cmd = 0.05, 0.0, 0.0
+x_vel_max, y_vel_max, yaw_vel_max = 1.5, 1.0, 3.0
+
+joystick_use = True
+joystick_opened = False
+def on_press(key):
+    global x_vel_cmd, y_vel_cmd, yaw_vel_cmd
+    try:
+        if key.char == '6':
+            x_vel_cmd +=0.1
+        elif key.char == '7':
+            x_vel_cmd -=0.1
+        elif key.char == '8':
+            y_vel_cmd += 0.3
+        elif key.char == '9':
+            y_vel_cmd -= 0.3
+        elif key.char == '-':
+            yaw_vel_cmd += 0.5
+        elif key.char == '=':
+            yaw_vel_cmd -= 0.5
+        elif key.char == '1':
+            x_vel_cmd=0
+            y_vel_cmd=0
+            yaw_vel_cmd=0
+        print(f"Updated velocities: vx={x_vel_cmd}, vy={y_vel_cmd}, dyaw={yaw_vel_cmd}")
+    except AttributeError:
+        pass
+
+class Deploy:
+    def __init__(self):
+        import argparse
+        listener = keyboard.Listener(on_press=on_press)
+        listener.start()
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("-c", "--config_file", type=str,
+                         default="m20.yaml",
+                         help="config file name in the config folder")
+        self.args = self.parser.parse_args()
+        self.config_file = self.args.config_file
+        with open(f"{DEPLOY_DIR}/deploy/deploy_mujoco/configs/{self.config_file}", "r") as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+            self.policy_path = config["policy_path"]
+            self.xml_path = config["xml_path"]
+            print(self.xml_path)
+            print(self.policy_path)
+
+            self.simulation_duration = config["simulation_duration"]
+            self.simulation_dt = config["simulation_dt"]
+            self.control_decimation = config["control_decimation"]
+
+            self.kps = np.array(config["kps"], dtype=np.float32)
+            self.kds = np.array(config["kds"], dtype=np.float32)
+
+            self.default_angles = np.array(config["default_angles"], dtype=np.float32)
+            self.torque_limits = np.array(config["torque_limits"], dtype=np.float32)
+
+            self.ang_vel_scale = config["ang_vel_scale"]
+            self.dof_pos_scale = config["dof_pos_scale"]
+            self.dof_vel_scale = config["dof_vel_scale"]
+            self.action_scale = config["action_scale"]
+            self.cmd_scale = np.array(config["cmd_scale"], dtype=np.float32)
+            self.vel_scale = config["vel_scale"]
+
+            self.num_actions = config["num_actions"]
+            self.num_obs = config["num_obs"]
+
+            self.wheel_indices = config["wheel_sim_indices"]
+            self.cmd = np.array(config["cmd_init"], dtype=np.float32)
+
+        # define context variables
+        self.action = np.zeros(self.num_actions, dtype=np.float32)
+        self.target_dof_pos = self.default_angles.copy()
+        self.obs = np.zeros(self.num_obs, dtype=np.float32)
+        self.obs_hist_buf = np.zeros(self.num_obs * 6, dtype=np.float32)
+
+        self.dof_pos = np.zeros(self.num_actions, dtype=np.float32)
+        self.dof_vel = np.zeros(self.num_actions, dtype=np.float32)
+        self.quat = np.zeros(4, dtype=np.float32)
+        self.ang_vel = np.zeros(3, dtype=np.float32)
+        self.line_vel = np.zeros(3, dtype=np.float32)
+
+        self.counter = 0
+
+        # Load robot model
+        self.m = mujoco.MjModel.from_xml_path(self.xml_path)
+        self.d = mujoco.MjData(self.m)
+        self.m.opt.timestep = self.simulation_dt
+
+        # load policy
+        # self.policy = torch.jit.load(self.policy_path)
+        self.policy = ort.InferenceSession(self.policy_path, provifers=["CPUExecutionProvider"])
+    
+    def get_gravity_orientation(self, quaternion):
+        qw = quaternion[0]
+        qx = quaternion[1]
+        qy = quaternion[2]
+        qz = quaternion[3]
+
+        gravity_orientation = np.zeros(3)
+
+        gravity_orientation[0] = 2 * (-qz * qx + qw * qy)
+        gravity_orientation[1] = -2 * (qz * qy + qw * qx)
+        gravity_orientation[2] = 1 - 2 * (qw * qw + qz * qz)
+
+        return gravity_orientation
+    
+    def get_robot_state(self):
+        self.dof_pos = self.d.qpos[7:]
+        self.dof_vel = self.d.qvel[6:]
+        self.quat = self.d.qpos[3:7]
+        self.ang_vel = self.d.qvel[3:6]
+        self.line_vel = self.d.qvel[3:6]
+    
+    def compute_torques(self, actions):
+        dof_err = self.default_angles - self.dof_pos # 各DOF默认位置 - 目前各DOF位置
+        dof_err[self.wheel_indices] =  0 # 轮子的误差是0
+
+        actions_scaled = actions * self.action_scale # action * 0.25
+        actions_scaled[self.wheel_indices] = 0 # 轮子使用速度控制，角度增量为0
+        vel_ref = np.zeros_like(actions_scaled)
+        vel_tmp = actions * self.vel_scale # action提供期望速度
+        vel_ref[self.wheel_indices] = vel_tmp[self.wheel_indices] # 只有轮子使用速度控制
+        
+        
+        torques = self.kps * (
+                actions_scaled + dof_err
+            ) + self.kds * (vel_ref - self.dof_vel)
+        
+    
+        return np.clip(torques, -self.torque_limits, self.torque_limits)
+    
+    def compute_observation(self):
+        dof_error = (self.dof_pos - self.default_angles) * self.dof_pos_scale
+        dof_error[self.wheel_indices] = 0
+
+        dof_pos = self.dof_pos.copy()
+        dof_pos[self.wheel_indices] = 0
+
+        dof_vel = self.dof_vel * self.dof_vel_scale
+        gravity_orientation = self.get_gravity_orientation(self.quat)
+        omega = self.ang_vel * self.ang_vel_scale
+        self.obs[0] =x_vel_cmd * self.cmd_scale[0]
+        self.obs[1] =y_vel_cmd * self.cmd_scale[1]
+        self.obs[2] =yaw_vel_cmd * self.cmd_scale[2]
+        self.obs[3:6] = omega
+        self.obs[6:9] = gravity_orientation
+        self.obs[9 : 9 + self.num_actions] = dof_error
+        self.obs[9 + self.num_actions : 9 + 2 * self.num_actions] = dof_vel
+        self.obs[9 + 2 * self.num_actions : 9 + 3 * self.num_actions] =self.action 
+        
+        self.obs_hist_buf = self.obs_hist_buf[57:]
+        self.obs_hist_buf = np.concatenate((self.obs_hist_buf, self.obs), axis=-1)
+
+
+    def run_sim(self):
+        self.vel_data = []
+        self.cmd_data = []
+        with mujoco.viewer.launch_passive(self.m, self.d) as viewer:
+            # Close the viewer automatically after simulation_duration wall-seconds.
+            start = time.time()
+            
+            while viewer.is_running() and time.time() - start < self.simulation_duration:
+                step_start = time.time()
+                tau = self.compute_torques(self.action)
+                self.d.ctrl[:] = tau
+                # mj_step can be replaced with code that also evaluates
+                # a policy and applies a control signal before stepping the physics.
+                mujoco.mj_step(self.m, self.d)
+
+                self.counter += 1
+                if self.counter % self.control_decimation == 0:
+                    self.get_robot_state()
+                    self.compute_observation()
+                    
+                    out_name = self.policy.get_outputs()[0].name
+                    actions = self.policy.run([out_name], {"obs": self.obs_hist_buf.reshape(1, -1)})
+                    self.action = actions[0][0]
+
+                    self.vel_data.append(self.dof_pos.copy())
+                    
+                viewer.sync()
+                # Rudimentary time keeping, will drift relative to wall clock.
+                time_until_next_step = self.m.opt.timestep - (time.time() - step_start)
+                if time_until_next_step > 0:
+                    time.sleep(time_until_next_step)
+
+if __name__ == "__main__":
+    deploy = Deploy()
+
+    deploy.run_sim()
